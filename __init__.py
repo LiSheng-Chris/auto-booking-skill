dce24cc613dab95e82f271548462cfa716872044
lisdre <lisheng0714@gmail.com>
2021-04-19 22:07:15 +0800

    camera

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
+from picamera import PiCamera
+from time import sleep
 
 
 class AutoBooking(MycroftSkill):
@@ -7,6 +9,11 @@ class AutoBooking(MycroftSkill):
 
     @intent_file_handler('booking.auto.intent')
     def handle_booking_auto(self, message):
+        camera = PiCamera()
+        camera.start_preview()
+        sleep(3)
+        camera.capture('/home/pi/ISAPM/temp/image.jpg')
+        camera.stop_preview()
         bookingDate = self.get_response('Which date you want to booking')
         self.speak_dialog(bookingDate)
 
