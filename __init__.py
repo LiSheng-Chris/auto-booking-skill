5700a820a89a61b3ea822eb40c401a512c76fcee
lisdre <lisheng0714@gmail.com>
2021-04-19 21:38:30 +0800

    text camera

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
+from picamera import PiCamera
+from time import sleep
 
 
 class AutoBooking(MycroftSkill):
@@ -7,9 +9,13 @@ class AutoBooking(MycroftSkill):
 
     @intent_file_handler('booking.auto.intent')
     def handle_booking_auto(self, message):
-        favorite_flavor = self.get_response('booking.auto.dialog')
-        self.speak_dialog('confirm.favorite.flavor', {'flavor': favorite_flavor})
-
+        camera = PiCamera()
+        camera.start_preview()
+        sleep(3)
+        camera.capture('/home/pi/ISAPM/temp/image.jpg')
+        camera.stop_preview()
+        bookingDate = self.get_response('Which date you want to booking')
+        self.speak_dialog(bookingDate)
 
 def create_skill():
     return AutoBooking()
