b14f3bc326ae65dbc14c31975ee6a197675cdae8
gu-lijian <gulijian@gmail.com>
2021-04-26 23:42:24 +0800

    show image log

--- __init__.py
+++ __init__.py
@@ -25,8 +25,9 @@ class AutoBooking(MycroftSkill):
         ## Lijian part end
         
         ## Li Sheng part start
+        self.speak_dialog("Hi, prepare to show image.")
         self.gui.show_image("https://placeimg.com/500/300/nature")
-
+        self.speak_dialog("Hi, show image is finished!")
         firstName = self.get_response("What is you first name")
         lastName = self.get_response("What is you last name")
         self.speak_dialog("Hi " + firstName + " " + lastName)
